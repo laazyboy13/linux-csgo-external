bfa80f5b56ce2c222743eecb9e047a7c1e1549ea
laazyboy13 <laazyboy13@gmail.com>
2015-11-13 00:12:53 -1000

    first commit, started with recode

---
+++ logger/logger.cpp
@@ -0,0 +1,6 @@
+/*
+   Created by laazyboy13 on 11/12/15 11:12 PM.
+   Credits: s0beit - original linux external glow esp
+*/
+
+#include "logger.hpp"
--- logger/logger.hpp
+++ logger/logger.hpp
@@ -1,5 +1,7 @@
 /*
-   Created by laazyboy13 on 11/12/15 9:31 PM.
+   Created by laazyboy13 on 11/12/15 11:12 PM.
    Credits: s0beit - original linux external glow esp
 */
 
+
+#pragma once
