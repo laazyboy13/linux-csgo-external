534deebbaf4c3808c77f6ba2c4bcb6620220d20b
LuK1337 <priv.luk@gmail.com>
2016-09-04 13:41:35 +0200

    Fix code style
    * Use tabs everywhere
    * Remove unused variables

--- hack.cpp
+++ hack.cpp
@@ -6,162 +6,140 @@ struct hack::GlowObjectDefinition_t g_glow[1024];
 int count = 0;
 unsigned char spotted = 1;
 
-void NoFlash(remote::Handle* csgo, remote::MapModuleMemoryRegion* client, unsigned long localPlayer)
-{
-    float fFlashAlpha = 70.0f;
-    float fFlashAlphaFull = 255.0f;
-    float fFlashAlphaFromGame = 0.0f;
+void NoFlash(remote::Handle* csgo, remote::MapModuleMemoryRegion* client, unsigned long localPlayer) {
+	float fFlashAlpha = 70.0f;
+	float fFlashAlphaFull = 255.0f;
+	float fFlashAlphaFromGame = 0.0f;
 
-    csgo->Read((void*) (localPlayer+0xABE4), &fFlashAlphaFromGame, sizeof(float));
-    if((fFlashAlphaFromGame > fFlashAlpha) && (csgo->m_bShouldNoFlash == true))
-	csgo->Write((void*) (localPlayer+0xABE4), &fFlashAlpha, sizeof(float));
-    else if ((fFlashAlphaFromGame < fFlashAlphaFull) && (csgo->m_bShouldNoFlash == false))
-	csgo->Write((void*) (localPlayer+0xABE4), &fFlashAlphaFull, sizeof(float));
-   
+	csgo->Read((void*) (localPlayer+0xABE4), &fFlashAlphaFromGame, sizeof(float));
+	if ((fFlashAlphaFromGame > fFlashAlpha) && (csgo->m_bShouldNoFlash))
+		csgo->Write((void*) (localPlayer+0xABE4), &fFlashAlpha, sizeof(float));
+	else if ((fFlashAlphaFromGame < fFlashAlphaFull) && (!csgo->m_bShouldNoFlash))
+		csgo->Write((void*) (localPlayer+0xABE4), &fFlashAlphaFull, sizeof(float));
 }
 
-void hack::Bhop(remote::Handle* csgo, remote::MapModuleMemoryRegion* client, Display* display)
-{
-    if (!csgo || !client)
-        return;
-    
-    int keycodeJump = XKeysymToKeycode(display, XK_K);
-    
-    unsigned long localPlayer = 0;
+void hack::Bhop(remote::Handle* csgo, remote::MapModuleMemoryRegion* client, Display* display) {
+	if (!csgo || !client)
+		return;
 
-    csgo->Read((void*) csgo->m_addressOfLocalPlayer, &localPlayer, sizeof(long));
-    
-    unsigned int onGround = 0;
-    csgo->Read((void*) (localPlayer+0x134+0x4), &onGround, sizeof(int));
-    
-    onGround = onGround & (1 << 0);
-    
-    if (onGround == 1 && csgo->m_bShouldBHop && csgo->m_bBhopEnabled)
-    {
-        unsigned int jump = 5;
-        csgo->Write((void*) (csgo->m_oAddressOfForceJump), &jump, sizeof(int));
-        this_thread::sleep_for(chrono::milliseconds(2));
-        jump = 4;
-        csgo->Write((void*) (csgo->m_oAddressOfForceJump), &jump, sizeof(int));
-    }
+	unsigned long localPlayer = 0;
+
+	csgo->Read((void*) csgo->m_addressOfLocalPlayer, &localPlayer, sizeof(long));
+
+	unsigned int onGround = 0;
+	csgo->Read((void*) (localPlayer+0x134+0x4), &onGround, sizeof(int));
+
+	onGround = onGround & (1 << 0);
+
+	if (onGround == 1 && csgo->m_bShouldBHop && csgo->m_bBhopEnabled) {
+		unsigned int jump = 5;
+		csgo->Write((void*) (csgo->m_oAddressOfForceJump), &jump, sizeof(int));
+		this_thread::sleep_for(chrono::milliseconds(2));
+		jump = 4;
+		csgo->Write((void*) (csgo->m_oAddressOfForceJump), &jump, sizeof(int));
+	}
 }
 
 void hack::Glow(remote::Handle* csgo, remote::MapModuleMemoryRegion* client) {
-    if (!csgo || !client)
-        return;
+	if (!csgo || !client)
+		return;
 
-    // Reset
-    bzero(g_remote, sizeof(g_remote));
-    bzero(g_local, sizeof(g_local));
-    bzero(g_glow, sizeof(g_glow));
+	// Reset
+	bzero(g_remote, sizeof(g_remote));
+	bzero(g_local, sizeof(g_local));
+	bzero(g_glow, sizeof(g_glow));
 
-    hack::CGlowObjectManager manager;
+	hack::CGlowObjectManager manager;
 
-    if (!csgo->Read((void*) csgo->m_addressOfGlowPointer, &manager, sizeof(hack::CGlowObjectManager)))
-	{
+	if (!csgo->Read((void*) csgo->m_addressOfGlowPointer, &manager, sizeof(hack::CGlowObjectManager))) {
 		Logger::error ("Failed to read glowClassAddress");
-        throw 1;
-        return;
-    }
+		throw 1;
+		return;
+	}
 
-    size_t count = manager.m_GlowObjectDefinitions.Count;
+	size_t count = manager.m_GlowObjectDefinitions.Count;
 
-    void* data_ptr = (void*) manager.m_GlowObjectDefinitions.DataPtr;
+	void* data_ptr = (void*) manager.m_GlowObjectDefinitions.DataPtr;
 
-    if (!csgo->Read(data_ptr, g_glow, sizeof(hack::GlowObjectDefinition_t) * count))
-	{
+	if (!csgo->Read(data_ptr, g_glow, sizeof(hack::GlowObjectDefinition_t) * count)) {
 		Logger::error ("Failed to read m_GlowObjectDefinitions");
-        throw 1;
-        return;
-    }
+		throw 1;
+		return;
+	}
 
-    size_t writeCount = 0;
-    unsigned long localPlayer = 0;
-    unsigned int teamNumber = 0;
+	size_t writeCount = 0;
+	unsigned long localPlayer = 0;
+	unsigned int teamNumber = 0;
 
-    csgo->Read((void*) csgo->m_addressOfLocalPlayer, &localPlayer, sizeof(long));
+	csgo->Read((void*) csgo->m_addressOfLocalPlayer, &localPlayer, sizeof(long));
 
-    if(localPlayer != 0)
-    {
-        csgo->Read((void*) (localPlayer+0x128), &teamNumber, sizeof(int));
-        NoFlash(csgo, client, localPlayer);	
-    }
-    
-    for (unsigned int i = 0; i < count; i++)
-    {
-        if (g_glow[i].m_pEntity != NULL)
-        {
-            Entity ent;
-            
-            if (csgo->Read(g_glow[i].m_pEntity, &ent, sizeof(Entity)))
-            {
-                
-                if (ent.m_iTeamNum != 2 && ent.m_iTeamNum != 3)
-                {
-                    g_glow[i].m_bRenderWhenOccluded = 0;
-                    g_glow[i].m_bRenderWhenUnoccluded = 0;
-                    continue;
-                }
-                        
-                unsigned int iAlt1Status = 0 ;
-                csgo->Read((void*) (csgo->m_addressOfAlt1), &iAlt1Status, sizeof(int)); 
+	if (localPlayer != 0) {
+		csgo->Read((void*) (localPlayer+0x128), &teamNumber, sizeof(int));
+		NoFlash(csgo, client, localPlayer);
+	}
 
-                if(localPlayer != 0 && (iAlt1Status == 0x5) )
-                {
-                    if(ent.m_iTeamNum != teamNumber)
-                    {
-                        unsigned int crossHairId = 0;
-                        unsigned int entityId = 0;
-                        unsigned int attack = 0x5;
-                        unsigned int release = 0x4;
-                        csgo->Read((void*) (localPlayer+0xB380), &crossHairId, sizeof(int));
-                        csgo->Read((void*) (g_glow[i].m_pEntity + 0x94), &entityId, sizeof(int));
-                        
-                        if(crossHairId == entityId)
-                        {
-                            usleep(100);
-                            csgo->Write((void*) (csgo->m_addressOfForceAttack), &attack, sizeof(int));
-                            usleep(100);
-                            csgo->Write((void*) (csgo->m_addressOfForceAttack), &release, sizeof(int));
-                        }
-                    }
-                }
+	for (unsigned int i = 0; i < count; i++) {
+		if (g_glow[i].m_pEntity != NULL) {
+			Entity ent;
 
-                csgo->Write((void*) ((unsigned long) g_glow[i].m_pEntity + 0xEC5), &spotted, sizeof(unsigned char));                
+			if (csgo->Read(g_glow[i].m_pEntity, &ent, sizeof(Entity))) {
+				if (ent.m_iTeamNum != 2 && ent.m_iTeamNum != 3) {
+					g_glow[i].m_bRenderWhenOccluded = 0;
+					g_glow[i].m_bRenderWhenUnoccluded = 0;
+					continue;
+				}
 
-                if(g_glow[i].m_bRenderWhenOccluded == 1)
-				{
-                    continue;
-                }
+				unsigned int iAlt1Status = 0 ;
+				csgo->Read((void*) (csgo->m_addressOfAlt1), &iAlt1Status, sizeof(int));
 
-                        
-                g_glow[i].m_bRenderWhenOccluded = 1;
-                g_glow[i].m_bRenderWhenUnoccluded = 0;
-                
-				if (ent.m_iTeamNum == 2 || ent.m_iTeamNum == 3)
-				{
-                    g_glow[i].m_flGlowRed = (teamNumber != ent.m_iTeamNum ? 1.0f : 0.0f);
-                    g_glow[i].m_flGlowGreen = 0.0f;
-                    g_glow[i].m_flGlowBlue = (teamNumber == ent.m_iTeamNum ? 1.0f : 0.0f);
-                    g_glow[i].m_flGlowAlpha = 0.6f;
-                }
-            }
-        }
-        
-        if(csgo->m_bShouldGlow)
-        {
-            size_t bytesToCutOffEnd = sizeof(hack::GlowObjectDefinition_t) - g_glow[i].writeEnd();
-            size_t bytesToCutOffBegin = (size_t) g_glow[i].writeStart();
-            size_t totalWriteSize = (sizeof(hack::GlowObjectDefinition_t) - (bytesToCutOffBegin + bytesToCutOffEnd));
+				if (localPlayer != 0 && (iAlt1Status == 0x5)) {
+					if (ent.m_iTeamNum != teamNumber) {
+						unsigned int crossHairId = 0;
+						unsigned int entityId = 0;
+						unsigned int attack = 0x5;
+						unsigned int release = 0x4;
+						csgo->Read((void*) (localPlayer+0xB380), &crossHairId, sizeof(int));
+						csgo->Read((void*) (g_glow[i].m_pEntity + 0x94), &entityId, sizeof(int));
 
-            g_remote[writeCount].iov_base =
-                    ((uint8_t*) data_ptr + (sizeof(hack::GlowObjectDefinition_t) * i)) + bytesToCutOffBegin;
-            g_local[writeCount].iov_base = ((uint8_t*) &g_glow[i]) + bytesToCutOffBegin;
-            g_remote[writeCount].iov_len = g_local[writeCount].iov_len = totalWriteSize;
+						if (crossHairId == entityId) {
+							usleep(100);
+							csgo->Write((void*) (csgo->m_addressOfForceAttack), &attack, sizeof(int));
+							usleep(100);
+							csgo->Write((void*) (csgo->m_addressOfForceAttack), &release, sizeof(int));
+						}
+					}
+				}
 
-            writeCount++;
-        }
-    }
-    
-    process_vm_writev(csgo->GetPid(), g_local, writeCount, g_remote, writeCount, 0);
+				csgo->Write((void*) ((unsigned long) g_glow[i].m_pEntity + 0xEC5), &spotted, sizeof(unsigned char));
+
+				if (g_glow[i].m_bRenderWhenOccluded == 1)
+					continue;
+
+				g_glow[i].m_bRenderWhenOccluded = 1;
+				g_glow[i].m_bRenderWhenUnoccluded = 0;
+
+				if (ent.m_iTeamNum == 2 || ent.m_iTeamNum == 3) {
+					g_glow[i].m_flGlowRed = (teamNumber != ent.m_iTeamNum ? 1.0f : 0.0f);
+					g_glow[i].m_flGlowGreen = 0.0f;
+					g_glow[i].m_flGlowBlue = (teamNumber == ent.m_iTeamNum ? 1.0f : 0.0f);
+					g_glow[i].m_flGlowAlpha = 0.6f;
+				}
+			}
+		}
+
+		if (csgo->m_bShouldGlow) {
+			size_t bytesToCutOffEnd = sizeof(hack::GlowObjectDefinition_t) - g_glow[i].writeEnd();
+			size_t bytesToCutOffBegin = (size_t) g_glow[i].writeStart();
+			size_t totalWriteSize = (sizeof(hack::GlowObjectDefinition_t) - (bytesToCutOffBegin + bytesToCutOffEnd));
+
+			g_remote[writeCount].iov_base =
+				((uint8_t*) data_ptr + (sizeof(hack::GlowObjectDefinition_t) * i)) + bytesToCutOffBegin;
+			g_local[writeCount].iov_base = ((uint8_t*) &g_glow[i]) + bytesToCutOffBegin;
+			g_remote[writeCount].iov_len = g_local[writeCount].iov_len = totalWriteSize;
+
+			writeCount++;
+		}
+	}
+
+	process_vm_writev(csgo->GetPid(), g_local, writeCount, g_remote, writeCount, 0);
 }
--- main.cpp
+++ main.cpp
@@ -26,201 +26,190 @@ using namespace std;
 #define SHOW_CSGO_LINUX_TITLE true
 #define CSGO_LINUX_TITLE_LOCATION "csgo-external-title"
 
-int main()
-{
-    Logger::init ();
-    
-    if (getuid() != 0)
-    {
-        Logger::error (string("Cannot start linux-csgo-external as ") + UNDERLINE + "NON ROOT" RESET RED " user.");
-        return 0;
-    }
-    
+int main() {
+	Logger::init ();
+
+	if (getuid() != 0) {
+		Logger::error (string("Cannot start linux-csgo-external as ") + UNDERLINE + "NON ROOT" RESET RED " user.");
+		return 0;
+	}
+
 #if SHOW_CSGO_LINUX_TITLE
-    system ("cat " CSGO_LINUX_TITLE_LOCATION);
+	system ("cat " CSGO_LINUX_TITLE_LOCATION);
 #endif
 
-    cout << UNDERLINE "\n                                                   " RESET << endl;
-    cout << UNDERLINE "---------------[linux-csgo-external]---------------\n" RESET << endl;
-    cout << BOLD RED << "█ AUTHOR: " UNDERLINE WHITE "\ts0beit" RESET << endl;
-    cout << BOLD YELLOW << "█ MAINTAINER:" UNDERLINE WHITE "\tMcSwaggens" RESET << endl;
-    cout << BOLD GREEN << "█ MAINTAINER:" UNDERLINE WHITE "\towerosu" RESET << endl;
-    cout << BOLD CYAN << "█ PROGRAMMER:" UNDERLINE WHITE "\tluk1337" RESET << endl;
-    cout << BOLD BLUE << "█ HELP: " UNDERLINE WHITE "\tCommunity" RESET << endl;
-    cout << BOLD MAGENTA << "█ Github:" UNDERLINE WHITE "\thttps://github.com/McSwaggens/linux-csgo-external" RESET << endl;
-    cout << UNDERLINE "                                                   " RESET << endl;
-    cout << UNDERLINE "---------------[linux-csgo-external]---------------\n" RESET << endl;
-    
-    Display* display = XOpenDisplay(0);
+	cout << UNDERLINE "\n													" RESET << endl;
+	cout << UNDERLINE "---------------[linux-csgo-external]---------------\n" RESET << endl;
+	cout << BOLD RED << "█ AUTHOR: " UNDERLINE WHITE "\ts0beit" RESET << endl;
+	cout << BOLD YELLOW << "█ MAINTAINER:" UNDERLINE WHITE "\tMcSwaggens" RESET << endl;
+	cout << BOLD GREEN << "█ MAINTAINER:" UNDERLINE WHITE "\towerosu" RESET << endl;
+	cout << BOLD CYAN << "█ PROGRAMMER:" UNDERLINE WHITE "\tluk1337" RESET << endl;
+	cout << BOLD BLUE << "█ HELP: " UNDERLINE WHITE "\tCommunity" RESET << endl;
+	cout << BOLD MAGENTA << "█ Github:" UNDERLINE WHITE "\thttps://github.com/McSwaggens/linux-csgo-external" RESET << endl;
+	cout << UNDERLINE "													  " RESET << endl;
+	cout << UNDERLINE "---------------[linux-csgo-external]---------------\n" RESET << endl;
+
+	Display* display = XOpenDisplay(0);
 	Window root = DefaultRootWindow(display);
 
-    int keycodeGlow = XKeysymToKeycode(display, XK_F7);
-    int keycodeFlash = XKeysymToKeycode(display, XK_F8);
-    int keycodeBHopEnable = XKeysymToKeycode(display, XK_F9);
-    int keycodeBHop = XKeysymToKeycode(display, XK_space);
+	int keycodeGlow = XKeysymToKeycode(display, XK_F7);
+	int keycodeFlash = XKeysymToKeycode(display, XK_F8);
+	int keycodeBHopEnable = XKeysymToKeycode(display, XK_F9);
+	int keycodeBHop = XKeysymToKeycode(display, XK_space);
 
-    remote::Handle csgo;
+	remote::Handle csgo;
 
-    while (true)
-    {
-        if (remote::FindProcessByName("csgo_linux64", &csgo)) {
-            break;
-        }
+	while (true) {
+		if (remote::FindProcessByName("csgo_linux64", &csgo))
+			break;
 
-        usleep(500);
-    }
-    
-    stringstream ss;
-    ss << "\t  CSGO Process UID:\t [";
-    ss << csgo.GetPid ();
-    ss << "]";
-    
-    Logger::normal (ss.str());
+		usleep(500);
+	}
 
-    remote::MapModuleMemoryRegion client;
+	stringstream ss;
+	ss << "\t  CSGO Process UID:\t [";
+	ss << csgo.GetPid ();
+	ss << "]";
 
-    client.start = 0;
+	Logger::normal (ss.str());
 
-    while (client.start == 0)
-    {
-        if (!csgo.IsRunning())
-        {
+	remote::MapModuleMemoryRegion client;
+
+	client.start = 0;
+
+	while (client.start == 0) {
+		if (!csgo.IsRunning()) {
 			Logger::error ("The game was closed before I could find the client library inside of csgo");
-            return 0;
-        }
+			return 0;
+		}
 
-        csgo.ParseMaps();
+		csgo.ParseMaps();
 
-        for (auto region : csgo.regions)
-        {
-            if (region.filename.compare("client_client.so") == 0 && region.executable)
-            {
-                client = region;
-                break;
-            }
-        }
+		for (auto region : csgo.regions) {
+			if (region.filename.compare("client_client.so") == 0 && region.executable) {
+				client = region;
+				break;
+			}
+		}
 
-        usleep(500);
-    }
-    
-    client.client_start = client.start;
-    
-    unsigned long pEngine = remote::getModule("engine_client.so", csgo.GetPid());
-    
-    if (pEngine == 0)
-    {
+		usleep(500);
+	}
+
+	client.client_start = client.start;
+
+	unsigned long pEngine = remote::getModule("engine_client.so", csgo.GetPid());
+
+	if (pEngine == 0) {
 		Logger::error ("Couldn't find engine module inside of csgo");
-        return 0;
-    }
-    
-    csgo.a_engine_client = pEngine;
-    
-    Logger::address ("client_client.so:\t", client.start);
-    Logger::address ("engine_client.so:\t", pEngine);
-    
-    void* foundGlowPointerCall = client.find(csgo,
-                                             "\xE8\x00\x00\x00\x00\x48\x8b\x10\x48\xc1\xe3\x06\x44",
-                                             "x????xxxxxxxx");
-    
-    unsigned long call = csgo.GetCallAddress(foundGlowPointerCall);
-    
-    
-    Logger::address ("Glow function:\t", call);
-    
-    csgo.m_addressOfGlowPointer = csgo.GetCallAddress((void*)(call+0xF));
-    Logger::address ("Glow array pointer:\t", csgo.m_addressOfGlowPointer);
-    
-    long ptrLocalPlayer = (client.client_start + 0x5A9B1A0); // 27/06/16
-    unsigned long foundLocalPlayerLea = (long)client.find(csgo,
-                                             "\x48\x89\xe5\x74\x0e\x48\x8d\x05\x00\x00\x00\x00", //27/06/16
-                                             "xxxxxxxx????");
-                                             
-    csgo.m_addressOfLocalPlayer = csgo.GetCallAddress((void*)(foundLocalPlayerLea+0x7));
+		return 0;
+	}
 
-    unsigned long foundAttackMov = (long)client.find(csgo,
-                                             "\x44\x89\xe8\x83\xe0\x01\xf7\xd8\x83\xe8\x03\x45\x84\xe4\x74\x00\x21\xd0", //10/07/16
-                                             "xxxxxxxxxxxxxxx?xx");
-    csgo.m_addressOfForceAttack = csgo.GetCallAddress((void*)(foundAttackMov+19));
+	csgo.a_engine_client = pEngine;
 
-    unsigned long foundAlt1Mov = (long)client.find(csgo,
-                                             "\x44\x89\xe8\xc1\xe0\x11\xc1\xf8\x1f\x83\xe8\x03\x45\x84\xe4\x74\x00\x21\xd0", //10/07/16
-                                             "xxxxxxxxxxxxxxxx?xx");
-    
-    csgo.m_addressOfAlt1 = csgo.GetCallAddress((void*)(foundAlt1Mov+20));
-    Logger::address ("LocalPlayer address:\t", csgo.m_addressOfLocalPlayer);
-    
-    /*   0x7f114cc6f414:	and    eax,edx
-   0x7f114cc6f416:	mov    DWORD PTR [rip+0x55d10f0],eax        # 0x7f115224050c
-   0x7f114cc6f41c:	mov    edx,DWORD PTR [rip+0x55d10de]        # 0x7f1152240500
-=> 0x7f114cc6f422:	mov    eax,ebx
-   0x7f114cc6f424:	or     eax,0x2
-   0x7f114cc6f427:	test   dl,0x3
-   0x7f114cc6f42a:	cmovne ebx,eax
-   0x7f114cc6f42d:	mov    eax,r13d
-*/
-    unsigned long foundForceJumpMov = (long)client.find(csgo,
-                                             "\x44\x89\xe8\xc1\xe0\x1d\xc1\xf8\x1f\x83\xe8\x03\x45\x84\xe4\x74\x08\x21\xd0", //01/09/16
-                                             "xxxxxxxxxxxxxxxx?xx");
-    
-    csgo.m_oAddressOfForceJump = csgo.GetCallAddress((void*)(foundForceJumpMov+26));
-    Logger::address ("Force Jump:\t\t", csgo.m_oAddressOfForceJump);
+	Logger::address ("client_client.so:\t", client.start);
+	Logger::address ("engine_client.so:\t", pEngine);
 
-    csgo.m_bShouldGlow = true;
-    csgo.m_bShouldNoFlash = true;
-    csgo.m_bBhopEnabled = true;
-    csgo.m_bShouldBHop = false;
-    
-    XEvent ev;
-    
-    char keys[32];
-    char lastkeys[32];
+	void* foundGlowPointerCall = client.find(csgo,
+		"\xE8\x00\x00\x00\x00\x48\x8b\x10\x48\xc1\xe3\x06\x44",
+		"x????xxxxxxxx");
 
-    while (csgo.IsRunning()) {
-        XQueryKeymap(display, keys);
+	unsigned long call = csgo.GetCallAddress(foundGlowPointerCall);
 
-        for (unsigned i = 0; i < sizeof(keys); ++i) {
-            if (keys[i] != lastkeys[i]) {
-                // check which key got changed
-                for (unsigned j = 0, test = 1; j < 8; ++j, test *= 2) {
-                    // if the key was pressed, and it wasn't before, print this
-                    if ((keys[i] & test) &&
-                            ((keys[i] & test) != (lastkeys[i] & test))) {
-                        const int code = i * 8 + j;
 
-                        if (code == keycodeGlow) {
-                            csgo.m_bShouldGlow = !csgo.m_bShouldGlow;
-                            Logger::toggle ("ESP\t\t", csgo.m_bShouldGlow);
-                        } else if (code == keycodeFlash) {
-                            csgo.m_bShouldNoFlash = !csgo.m_bShouldNoFlash;
-                            Logger::toggle ("No Flash\t", csgo.m_bShouldNoFlash);
-                        } else if (code == keycodeBHopEnable) {
-                            csgo.m_bBhopEnabled = !csgo.m_bBhopEnabled;
-                            csgo.m_bShouldBHop = false;
+	Logger::address ("Glow function:\t", call);
+
+	csgo.m_addressOfGlowPointer = csgo.GetCallAddress((void*)(call+0xF));
+	Logger::address ("Glow array pointer:\t", csgo.m_addressOfGlowPointer);
+
+	long ptrLocalPlayer = (client.client_start + 0x5A9B1A0); // 27/06/16
+	unsigned long foundLocalPlayerLea = (long)client.find(csgo,
+		"\x48\x89\xe5\x74\x0e\x48\x8d\x05\x00\x00\x00\x00", //27/06/16
+		"xxxxxxxx????");
+
+	csgo.m_addressOfLocalPlayer = csgo.GetCallAddress((void*)(foundLocalPlayerLea+0x7));
+
+	unsigned long foundAttackMov = (long)client.find(csgo,
+		"\x44\x89\xe8\x83\xe0\x01\xf7\xd8\x83\xe8\x03\x45\x84\xe4\x74\x00\x21\xd0", //10/07/16
+		"xxxxxxxxxxxxxxx?xx");
+	csgo.m_addressOfForceAttack = csgo.GetCallAddress((void*)(foundAttackMov+19));
+
+	unsigned long foundAlt1Mov = (long)client.find(csgo,
+		"\x44\x89\xe8\xc1\xe0\x11\xc1\xf8\x1f\x83\xe8\x03\x45\x84\xe4\x74\x00\x21\xd0", //10/07/16
+		"xxxxxxxxxxxxxxxx?xx");
+
+	csgo.m_addressOfAlt1 = csgo.GetCallAddress((void*)(foundAlt1Mov+20));
+	Logger::address ("LocalPlayer address:\t", csgo.m_addressOfLocalPlayer);
+
+	/*
+		0x7f114cc6f414:	 and	eax,edx
+		0x7f114cc6f416:	 mov	DWORD PTR [rip+0x55d10f0],eax		 # 0x7f115224050c
+		0x7f114cc6f41c:	 mov	edx,DWORD PTR [rip+0x55d10de]		 # 0x7f1152240500
+	=>	0x7f114cc6f422:	 mov	eax,ebx
+		0x7f114cc6f424:	 or	eax,0x2
+		0x7f114cc6f427:	 test	dl,0x3
+		0x7f114cc6f42a:	 cmovne ebx,eax
+		0x7f114cc6f42d:	 mov	eax,r13d
+	*/
+	unsigned long foundForceJumpMov = (long)client.find(csgo,
+		"\x44\x89\xe8\xc1\xe0\x1d\xc1\xf8\x1f\x83\xe8\x03\x45\x84\xe4\x74\x08\x21\xd0", //01/09/16
+		"xxxxxxxxxxxxxxxx?xx");
+
+	csgo.m_oAddressOfForceJump = csgo.GetCallAddress((void*)(foundForceJumpMov+26));
+	Logger::address ("Force Jump:\t\t", csgo.m_oAddressOfForceJump);
+
+	csgo.m_bShouldGlow = true;
+	csgo.m_bShouldNoFlash = true;
+	csgo.m_bBhopEnabled = true;
+	csgo.m_bShouldBHop = false;
+
+	XEvent ev;
+
+	char keys[32];
+	char lastkeys[32];
+
+	while (csgo.IsRunning()) {
+		XQueryKeymap(display, keys);
+
+		for (unsigned i = 0; i < sizeof(keys); ++i) {
+			if (keys[i] != lastkeys[i]) {
+				// check which key got changed
+				for (unsigned j = 0, test = 1; j < 8; ++j, test *= 2) {
+					// if the key was pressed, and it wasn't before, print this
+					if ((keys[i] & test) &&
+							((keys[i] & test) != (lastkeys[i] & test))) {
+						const int code = i * 8 + j;
+
+						if (code == keycodeGlow) {
+							csgo.m_bShouldGlow = !csgo.m_bShouldGlow;
+							Logger::toggle ("ESP\t\t", csgo.m_bShouldGlow);
+						} else if (code == keycodeFlash) {
+							csgo.m_bShouldNoFlash = !csgo.m_bShouldNoFlash;
+							Logger::toggle ("No Flash\t", csgo.m_bShouldNoFlash);
+						} else if (code == keycodeBHopEnable) {
+							csgo.m_bBhopEnabled = !csgo.m_bBhopEnabled;
+							csgo.m_bShouldBHop = false;
 							Logger::toggle ("Bhop Lock\t", !csgo.m_bBhopEnabled);
-                        } else if (code == keycodeBHop) {
-                            if (csgo.m_bBhopEnabled) {
-                                csgo.m_bShouldBHop = !csgo.m_bShouldBHop;
-                                Logger::toggle ("Bhop\t\t", csgo.m_bShouldBHop);
-                            }
-                        }
-                    }
-                }
-            }
+						} else if (code == keycodeBHop) {
+							if (csgo.m_bBhopEnabled) {
+								csgo.m_bShouldBHop = !csgo.m_bShouldBHop;
+								Logger::toggle ("Bhop\t\t", csgo.m_bShouldBHop);
+							}
+						}
+					}
+				}
+			}
 
-            lastkeys[i] = keys[i];
-        }
-        
-        try
-        {
-            hack::Glow(&csgo, &client);
-            hack::Bhop(&csgo, &client, display);
-        }
-        catch (int exception)
-        {
-            Logger::error ("An error occurred, closing...");
-            break;
-        }
-    }
+			lastkeys[i] = keys[i];
+		}
 
-    return 0;
+		try {
+			hack::Glow(&csgo, &client);
+			hack::Bhop(&csgo, &client, display);
+		} catch (int exception) {
+			Logger::error ("An error occurred, closing...");
+			break;
+		}
+	}
+
+	return 0;
 }
